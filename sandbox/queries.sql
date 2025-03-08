d7afc3390caeb27fa5f5a8e7639caa06ca6b7b0d
Chip Locke <locke.giles@gmail.com>
2025-03-07 21:21:26 -0600

    starting the swtich to cultivated plans and fee schedules

--- sandbox/queries.sql
+++ sandbox/queries.sql
@@ -1,10 +1,39 @@
 -- Provider group wants to see thier rates for various things
-SELECT nr.service_line_id, sl.billing_code, nr.negotiated_rate, nr.negotiated_type, nr.expiration_date
+SELECT nrpg.provider_group_id, pg.payer_assigned_id, sl.billing_code, nr.negotiated_rate, nr.negotiated_type, nr.expiration_date
 FROM negotiated_rates nr
 JOIN service_lines sl ON nr.service_line_id = sl.id
 JOIN negotiated_rate_provider_groups nrpg ON nr.id = nrpg.negotiated_rate_id
-WHERE nrpg.provider_group_id = 27782;
+JOIN provider_groups pg on nrpg.provider_group_id = pg.id
+WHERE nrpg.provider_group_id = '988878372cdc0b21554f6e6cdb1d96b5a1ab76da9cbc6f5d5f253ebae4ced527';
 
+-- Same but based on a provider
+SELECT nrpg.provider_group_id, pg.payer_assigned_id, sl.billing_code, nr.negotiated_rate, nr.negotiated_type, nr.expiration_date
+FROM negotiated_rates nr
+JOIN service_lines sl ON nr.service_line_id = sl.id
+JOIN negotiated_rate_provider_groups nrpg ON nr.id = nrpg.negotiated_rate_id
+JOIN provider_groups pg on nrpg.provider_group_id = pg.id
+JOIN provider_group_members pgm on pg.id = pgm.provider_group_id
+JOIN providers pr on pgm.provider_id = pr.id
+WHERE pr.npi = '1093367575';
+
+-- Similar Plans
+SELECT 
+    ARRAY_AGG(p.name ORDER BY p.name) AS plan_names,  -- Group plans sharing same fee schedules
+    COUNT(p.id) AS num_plans,  -- Count how many plans share the same bundle
+    ARRAY_AGG(fs.description ORDER BY fs.description) AS fee_schedule_bundle
+FROM plans p
+JOIN plan_fee_schedules pfs ON p.id = pfs.plan_id
+JOIN fee_schedules fs ON pfs.fee_schedule_id = fs.id
+WHERE p.payer_id = 100
+GROUP BY p.category_id, p.category_id_type  -- Adjust grouping as needed
+ORDER BY num_plans DESC
+LIMIT 100;
+
+
+-- All provider groups with their member providers
+SELECT npi, payer_assigned_id, ein FROM provider_group_members pgm
+JOIN providers p on pgm.provider_id = p.id
+JOIN provider_groups pg on pgm.provider_group_id = pg.id
 
 -- How much a provider group is paid for a service, accross plans
 SELECT p.name AS plan_name, nr.negotiated_rate
@@ -26,6 +55,41 @@ JOIN provider_groups pg ON nrpg.provider_group_id = pg.id
 WHERE sl.billing_code = '73221'
 AND nr.payer_id = 100
 ORDER BY nr.negotiated_rate DESC;
+
+--Get all the rates defined for a plan
+SELECT 
+    p.name,
+	nr.negotiated_rate, 
+    nr.negotiated_type, 
+    nr.expiration_date, 
+    nr.billing_class, 
+    nr.service_line_id, 
+    sl.billing_code, 
+    sl.billing_code_type, 
+    sl.billing_code_type_version
+FROM negotiated_rates nr
+JOIN service_lines sl ON nr.service_line_id = sl.id
+JOIN plan_fee_schedules pfs ON nr.fee_schedule_id = pfs.fee_schedule_id
+JOIN plans p ON pfs.plan_id = p.id
+WHERE p.name = 'H15048 282 HMO TGE10 MET ST'
+
+--Group plans by linked fee_schedules
+WITH plan_fee_sets AS (
+    SELECT 
+        plan_id, 
+        array_agg(DISTINCT description ORDER BY description) AS fee_schedule_set
+    FROM plan_fee_schedules
+	JOIN fee_schedules fs on plan_fee_schedules.fee_schedule_id = fs.id
+    GROUP BY plan_id
+)
+SELECT 
+    fee_schedule_set, 
+	max(p.name),
+    COUNT(*) AS num_plans
+FROM plan_fee_sets
+JOIN plans p on plan_fee_sets.plan_id = p.id
+GROUP BY fee_schedule_set
+ORDER BY num_plans DESC;
 
 --Get plan details for p lans with fee schedule id 1805
 SELECT 
--- sandbox/setup.sql
+++ sandbox/setup.sql
@@ -18,6 +18,38 @@ CREATE TABLE payers (
 );
 
 CREATE TABLE plans (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,  
+    description TEXT,            
+    plan_type TEXT NOT NULL,     
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE fee_schedules (
+    id SERIAL PRIMARY KEY,
+    plan_id INT REFERENCES plans(id) ON DELETE CASCADE,  -- Links to the curated display plan
+    service_code TEXT NOT NULL,  -- CPT, HCPCS, DRG, etc.
+    billing_class TEXT,  -- Professional, Facility, etc.
+    negotiated_rate NUMERIC,  -- Fully built reimbursement rate
+    provider_type TEXT,  -- Physician, Hospital, Imaging Center, etc.
+    geography TEXT,  -- If applicable (state, region)
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE providers (
+    id TEXT PRIMARY KEY,
+    npi BIGINT UNIQUE NOT NULL
+);
+
+CREATE TABLE service_lines (
+    id TEXT PRIMARY KEY,
+    billing_code TEXT NOT NULL,
+    billing_code_type TEXT NOT NULL,
+    billing_code_type_version TEXT
+);
+
+
+CREATE TABLE subplans (
     id TEXT PRIMARY KEY,
     payer_id INT REFERENCES payers(id),
     name TEXT NOT NULL,
@@ -27,10 +59,6 @@ CREATE TABLE plans (
     plan_market_type TEXT
 );
 
-CREATE TABLE providers (
-    id TEXT PRIMARY KEY,
-    npi BIGINT UNIQUE NOT NULL
-);
 
 CREATE TABLE provider_groups (
     id TEXT PRIMARY KEY,
@@ -45,24 +73,17 @@ CREATE TABLE provider_group_members (
     PRIMARY KEY (provider_group_id, provider_id)
 );
 
-CREATE TABLE fee_schedules (
+CREATE TABLE machine_readable_files (
     id TEXT PRIMARY KEY,
     payer_id INT REFERENCES payers(id),
     description TEXT NOT NULL,
     source_file_url TEXT
 );
 
-CREATE TABLE plan_fee_schedules (
+CREATE TABLE machine_readable_file_links (
     id BIGSERIAL PRIMARY KEY,
-    plan_id TEXT REFERENCES plans(id),
-    fee_schedule_id TEXT REFERENCES fee_schedules(id)
-);
-
-CREATE TABLE service_lines (
-    id TEXT PRIMARY KEY,
-    billing_code TEXT NOT NULL,
-    billing_code_type TEXT NOT NULL,
-    billing_code_type_version TEXT
+    subplan_id TEXT REFERENCES subplans(id),
+    machine_readable_file_id TEXT REFERENCES machine_readable_files(id)
 );
 
 CREATE TABLE negotiated_rates (
@@ -70,11 +91,11 @@ CREATE TABLE negotiated_rates (
     payer_id INT REFERENCES payers(id),
     fee_schedule_id TEXT REFERENCES fee_schedules(id),
     service_line_id TEXT REFERENCES service_lines(id),
-    negotiation_arrangement TEXT NOT NULL,
-    negotiated_type TEXT NOT NULL,
-    negotiated_rate DECIMAL(10,2) NOT NULL,
+    negotiation_arrangement TEXT,
+    negotiated_type TEXT,
+    negotiated_rate DECIMAL(10,2),
     expiration_date DATE,
-    billing_class TEXT NOT NULL,
+    billing_class TEXT,
     service_codes TEXT[]  -- Array of strings for service codes
 );
 
@@ -84,100 +105,12 @@ CREATE TABLE negotiated_rate_provider_groups (
     provider_group_id TEXT REFERENCES provider_groups(id)
 );
 
-CREATE INDEX idx_pfs_plan_fee_schedules ON plan_fee_schedules (plan_id, fee_schedule_id);
+CREATE INDEX idx_sp_subplan_mrf ON subplan_machine_readable_files (subplan_id, machine_readable_file_id);
 CREATE INDEX idx_nrpg_negotiated_rate_provider ON negotiated_rate_provider_groups (negotiated_rate_id, provider_group_id);
 
 
 -- Insert data
 INSERT INTO payers (id, name, mrf_index_file_url) VALUES
-(100, 'Blue Cross and Blue Shield of Illinois', 'https://app0004702110a5prdnc868.blob.core.windows.net/toc/2025-02-21_Blue-Cross-and-Blue-Shield-of-Illinois_index.json');
-
-INSERT INTO plans (id, payer_id, name, type, category_id, category_id_type, plan_market_type) VALUES
-(digest('100_000523 10 PPO+ NPP83323_XOF', 'sha256'), 100, '000523 10 PPO+ NPP83323_XOF', 'PPO', '36096', 'HIOS', 'group');
-
-INSERT INTO providers (id, npi) VALUES
-(99, 1003232992),
-(98, 1003823220),
-(97, 1023161940),
-(96, 1033327069),
-(95, 1003882374),
-(94, 1013409903),
-(93, 1992092167);
-
-INSERT INTO provider_groups (id, payer_id, payer_assigned_id, ein) VALUES
-(999, 100, '121.1', '37-1488231'),
-(998, 100, '121.2', '36-3616314');
-
-INSERT INTO provider_group_members (provider_group_id, provider_id) VALUES
-(999, 99),
-(999, 98),
-(999, 97),
-(999, 96),
-(998, 95),
-(998, 94),
-(998, 93);
-
-INSERT INTO fee_schedules (id, payer_id, description, source_file_url) VALUES
-(10000, 100, 'PPO Participating Provider Options in-network file', 'https://app0004702110a5prdnc868.blob.core.windows.net/output/2025-02-17_Blue-Cross-and-Blue-Shield-of-Illinois_Blue-Advantage-HMO_in-network-rates.json.gz'),
-(10001, 100, 'HMO Illinois in-network file', 'https://app0004702110a5prdnc868.blob.core.windows.net/output/2025-02-17_Blue-Cross-and-Blue-Shield-of-Illinois_HMO-Illinois_in-network-rates.json.gz'),
-(10001, 100, 'Blue Advantage HMO in-network file', 'https://app0004702110a5prdnc868.blob.core.windows.net/output/2025-02-17_Blue-Cross-and-Blue-Shield-of-Illinois_Blue-Advantage-HMO_in-network-rates.json.gz');
-
-
-INSERT INTO plan_fee_schedules (id, plan_id, fee_schedule_id) VALUES
-(66, 500, 44);
-
-INSERT INTO service_lines (id, billing_code, billing_code_type, billing_code_type_version) VALUES
-(1, '93232', 'CPT', '2025'),
-(2, '73218', 'CPT', '2025');
-
-INSERT INTO negotiated_rates (id, payer_id, fee_schedule_id, service_line_id, negotiation_arrangement, negotiated_type, negotiated_rate, expiration_date, billing_class, service_codes) VALUES
-(37, 100, 44, 1, 'ffs', 'negotiated', 217.55, '2026-12-31', 'institutional', NULL),
-(38, 100, 44, 1, 'ffs', 'negotiated', 238.45, '2025-06-30', 'institutional', NULL),
-(39, 100, 44, 2, 'ffs', 'negotiated', 202.92, '2025-02-28', 'institutional', ARRAY['02', '17', '18', '19', '22']);
-
-INSERT INTO negotiated_rate_provider_groups (id, negotiated_rate_id, provider_group_id) VALUES
-(1, 37, 999),
-(2, 38, 998),
-(3, 39, 999),
-(4, 39, 998);
-
-
--- TO DROP ALL DATA
--- Disable constraints temporarily
-ALTER TABLE provider_group_members DROP CONSTRAINT provider_group_members_provider_group_id_fkey;
-ALTER TABLE provider_group_members DROP CONSTRAINT provider_group_members_provider_id_fkey;
-ALTER TABLE plan_fee_schedules DROP CONSTRAINT plan_fee_schedules_plan_id_fkey;
-ALTER TABLE plan_fee_schedules DROP CONSTRAINT plan_fee_schedules_fee_schedule_id_fkey;
-ALTER TABLE negotiated_rate_provider_groups DROP CONSTRAINT negotiated_rate_provider_groups_negotiated_rate_id_fkey;
-ALTER TABLE negotiated_rate_provider_groups DROP CONSTRAINT negotiated_rate_provider_groups_provider_group_id_fkey;
-ALTER TABLE negotiated_rates DROP CONSTRAINT negotiated_rates_payer_id_fkey;
-ALTER TABLE negotiated_rates DROP CONSTRAINT negotiated_rates_fee_schedule_id_fkey;
-ALTER TABLE negotiated_rates DROP CONSTRAINT negotiated_rates_service_line_id_fkey;
-
--- Truncate all tables
-TRUNCATE TABLE 
-    provider_group_members,
-    negotiated_rate_provider_groups,
-    negotiated_rates,
-    service_lines,
-    plan_fee_schedules,
-    fee_schedules,
-    provider_groups,
-    providers,
-    plans,
-    payers
-RESTART IDENTITY CASCADE;
-
--- Re-enable constraints
-ALTER TABLE provider_group_members ADD CONSTRAINT provider_group_members_provider_group_id_fkey FOREIGN KEY (provider_group_id) REFERENCES provider_groups(id);
-ALTER TABLE provider_group_members ADD CONSTRAINT provider_group_members_provider_id_fkey FOREIGN KEY (provider_id) REFERENCES providers(id);
-ALTER TABLE plan_fee_schedules ADD CONSTRAINT plan_fee_schedules_plan_id_fkey FOREIGN KEY (plan_id) REFERENCES plans(id);
-ALTER TABLE plan_fee_schedules ADD CONSTRAINT plan_fee_schedules_fee_schedule_id_fkey FOREIGN KEY (fee_schedule_id) REFERENCES fee_schedules(id);
-ALTER TABLE negotiated_rate_provider_groups ADD CONSTRAINT negotiated_rate_provider_groups_negotiated_rate_id_fkey FOREIGN KEY (negotiated_rate_id) REFERENCES negotiated_rates(id);
-ALTER TABLE negotiated_rate_provider_groups ADD CONSTRAINT negotiated_rate_provider_groups_provider_group_id_fkey FOREIGN KEY (provider_group_id) REFERENCES provider_groups(id);
-ALTER TABLE negotiated_rates ADD CONSTRAINT negotiated_rates_payer_id_fkey FOREIGN KEY (payer_id) REFERENCES payers(id);
-ALTER TABLE negotiated_rates ADD CONSTRAINT negotiated_rates_fee_schedule_id_fkey FOREIGN KEY (fee_schedule_id) REFERENCES fee_schedules(id);
-ALTER TABLE negotiated_rates ADD CONSTRAINT negotiated_rates_service_line_id_fkey FOREIGN KEY (service_line_id) REFERENCES service_lines(id);
-
-
+(100, 'Blue Cross and Blue Shield of Illinois', 'https://app0004702110a5prdnc868.blob.core.windows.net/toc/2025-02-21_Blue-Cross-and-Blue-Shield-of-Illinois_index.json'),
+(200, 'United Healthcare', '');
 
